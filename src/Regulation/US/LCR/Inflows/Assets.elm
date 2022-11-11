66cd6be226f0b3390c713bce55926122f02bc312
Stephen Goldbaum <129341+stephengoldbaum@users.noreply.github.com>
2022-11-11 13:35:15 -0500

    Fixing Asset build

--- src/Regulation/US/LCR/Inflows/Assets.elm
+++ src/Regulation/US/LCR/Inflows/Assets.elm
@@ -11,8 +11,8 @@
    limitations under the License.
 -}
 
-module Regulation.US.LCR.Inflows.Assets exposing (..)
 
+module Regulation.US.LCR.Inflows.Assets exposing (..)
 
 import Regulation.US.FR2052A.DataTables.Inflows.Assets exposing (..)
 import Regulation.US.FR2052A.Fields.CollateralClass as CollateralClass
@@ -20,8 +20,9 @@ import Regulation.US.FR2052A.Fields.Insured as Insured
 import Regulation.US.FR2052A.Fields.MaturityBucket as MaturityBucket
 import Regulation.US.FR2052A.Fields.SubProduct as SubProduct
 import Regulation.US.LCR.AmountCalculations exposing (..)
-import Regulation.US.LCR.Rule exposing (applyRule)
 import Regulation.US.LCR.Basics exposing (Balance)
+import Regulation.US.LCR.Rule exposing (applyRule)
+
 
 type alias AssetAggregations =
     { rule_1_section_20_a_1_C : Balance
@@ -31,14 +32,28 @@ type alias AssetAggregations =
     , rule_107_section_33_d_1 : Balance
     }
 
-applyRules : List Assets -> AssetAggregations
-applyRules assets =
-    AssetAggregations 
-        (rule_1_section_20_a_1_C assets)
-        (rule_1_section_20_a_1 assets)
-        (rule_1_section_20_b_1 assets)
-        (rule_1_section_20_c_1 assets)
-        (rule_107_section_33_d_1 assets)
+
+
+--applyRules : List Assets -> AssetAggregations
+--applyRules assets =
+--    AssetAggregations
+--        (rule_1_section_20_a_1_C assets)
+--        (rule_1_section_20_a_1 assets)
+--        (rule_1_section_20_b_1 assets)
+--        (rule_1_section_20_c_1 assets)
+--        (rule_107_section_33_d_1 assets)
+
+
+applyRules : Assets -> List ( String, Float )
+applyRules flow =
+    List.concat
+        [ applyRule (match_rule_1_section_20_a_1_C flow) "33(c)" flow.marketValue
+        , applyRule (match_rule_1_section_20_a_1 flow) "33(d)(1)" flow.marketValue
+        , applyRule (match_rule_1_section_20_b_1 flow) "33(d)(2)" flow.marketValue
+        , applyRule (match_rule_1_section_20_c_1 flow) "33(d)(2)" flow.marketValue
+        , applyRule (match_rule_107_section_33_d_1 flow) "33(d)(2)" flow.marketValue
+        ]
+
 
 rule_1_section_20_a_1_C : List Assets -> Balance
 rule_1_section_20_a_1_C assets =
@@ -47,12 +62,14 @@ rule_1_section_20_a_1_C assets =
         |> List.map .marketValue
         |> List.sum
 
+
 rule_1_section_20_a_1 : List Assets -> Balance
 rule_1_section_20_a_1 assets =
     assets
         |> List.filter match_rule_1_section_20_a_1
         |> List.map .marketValue
         |> List.sum
+
 
 rule_1_section_20_b_1 : List Assets -> Balance
 rule_1_section_20_b_1 assets =
@@ -61,12 +78,14 @@ rule_1_section_20_b_1 assets =
         |> List.map .marketValue
         |> List.sum
 
+
 rule_1_section_20_c_1 : List Assets -> Balance
 rule_1_section_20_c_1 assets =
     assets
         |> List.filter match_rule_1_section_20_c_1
         |> List.map .marketValue
         |> List.sum
+
 
 rule_107_section_33_d_1 : List Assets -> Balance
 rule_107_section_33_d_1 assets =
@@ -81,20 +100,20 @@ rule_107_section_33_d_1 assets =
 match_rule_1_section_20_a_1_C : Assets -> Bool
 match_rule_1_section_20_a_1_C flow =
     List.member flow.product [ i_A_3 ]
-    -- Sub-Product: Not Currency and Coin
-    && (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
-    -- Maturity Bucket: Open
-    && (MaturityBucket.isOpen flow.maturityBucket)
-    -- Collateral Class: A-0-Q
-    && (CollateralClass.isCash flow.collateralClass)
-    -- Forward Start Amount: NULL
-    && (flow.forwardStartAmount == Nothing)
-    -- Forward Start Bucket: NULL
-    && (flow.forwardStartBucket == Nothing)
-    -- Encumbrance Type: Null
-    && (flow.encumbranceType == Nothing)
-    -- Treasury Control: Y
-    && (flow.treasuryControl == True)
+        -- Sub-Product: Not Currency and Coin
+        && (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
+        -- Maturity Bucket: Open
+        && MaturityBucket.isOpen flow.maturityBucket
+        -- Collateral Class: A-0-Q
+        && CollateralClass.isCash flow.collateralClass
+        -- Forward Start Amount: NULL
+        && (flow.forwardStartAmount == Nothing)
+        -- Forward Start Bucket: NULL
+        && (flow.forwardStartBucket == Nothing)
+        -- Encumbrance Type: Null
+        && (flow.encumbranceType == Nothing)
+        -- Treasury Control: Y
+        && (flow.treasuryControl == True)
 
 
 {-| (1) High-Quality Liquid Assets (Subpart C, §.20-.22)
@@ -102,18 +121,18 @@ match_rule_1_section_20_a_1_C flow =
 match_rule_1_section_20_a_1 : Assets -> Bool
 match_rule_1_section_20_a_1 flow =
     List.member flow.product [ i_A_1, i_A_2 ]
-    -- Sub-Product: Not Currency and Coin
-    && (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
-    -- Collateral Class: A-1-Q; A-2-Q; A-3-Q; A-4-Q; A-5-Q; S-1-Q; S-2-Q; S-3-Q; S-4-Q; CB-1-Q; CB-2-Q
-    && (CollateralClass.isHQLALevel1 flow.collateralClass && not (CollateralClass.isCash flow.collateralClass))
-    -- Forward Start Amount: NULL
-    && (flow.forwardStartAmount == Nothing)
-    -- Forward Start Bucket: NULL
-    && (flow.forwardStartBucket == Nothing)
-    -- Encumbrance Type: Null
-    && (flow.encumbranceType == Nothing)
-    -- Treasury Control: Y
-    && (flow.treasuryControl == True)
+        -- Sub-Product: Not Currency and Coin
+        && (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
+        -- Collateral Class: A-1-Q; A-2-Q; A-3-Q; A-4-Q; A-5-Q; S-1-Q; S-2-Q; S-3-Q; S-4-Q; CB-1-Q; CB-2-Q
+        && (CollateralClass.isHQLALevel1 flow.collateralClass && not (CollateralClass.isCash flow.collateralClass))
+        -- Forward Start Amount: NULL
+        && (flow.forwardStartAmount == Nothing)
+        -- Forward Start Bucket: NULL
+        && (flow.forwardStartBucket == Nothing)
+        -- Encumbrance Type: Null
+        && (flow.encumbranceType == Nothing)
+        -- Treasury Control: Y
+        && (flow.treasuryControl == True)
 
 
 {-| (1) High-Quality Liquid Assets (Subpart C, §.20-.22)
@@ -121,18 +140,18 @@ match_rule_1_section_20_a_1 flow =
 match_rule_1_section_20_b_1 : Assets -> Bool
 match_rule_1_section_20_b_1 flow =
     List.member flow.product [ i_A_1, i_A_2 ]
-    -- Sub-Product: Not Currency and Coin
-    && (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
-    -- Collateral Class: G-1-Q; G-2-Q; G-3-Q; S-5-Q; S-6-Q; S-7-Q; CB-3-Q
-    && (CollateralClass.isHQLALevel2A flow.collateralClass)
-    -- Forward Start Amount: NULL
-    && (flow.forwardStartAmount == Nothing)
-    -- Forward Start Bucket: NULL
-    && (flow.forwardStartBucket == Nothing)
-    -- Encumbrance Type: Null
-    && (flow.encumbranceType == Nothing)
-    -- Treasury Control: Y
-    && (flow.treasuryControl == True)
+        -- Sub-Product: Not Currency and Coin
+        && (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
+        -- Collateral Class: G-1-Q; G-2-Q; G-3-Q; S-5-Q; S-6-Q; S-7-Q; CB-3-Q
+        && CollateralClass.isHQLALevel2A flow.collateralClass
+        -- Forward Start Amount: NULL
+        && (flow.forwardStartAmount == Nothing)
+        -- Forward Start Bucket: NULL
+        && (flow.forwardStartBucket == Nothing)
+        -- Encumbrance Type: Null
+        && (flow.encumbranceType == Nothing)
+        -- Treasury Control: Y
+        && (flow.treasuryControl == True)
 
 
 {-| (1) High-Quality Liquid Assets (Subpart C, §.20-.22)
@@ -140,18 +159,18 @@ match_rule_1_section_20_b_1 flow =
 match_rule_1_section_20_c_1 : Assets -> Bool
 match_rule_1_section_20_c_1 flow =
     List.member flow.product [ i_A_1, i_A_2 ]
-    -- Sub-Product: Not Currency and Coin
-    && (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
-    -- Collateral Class: E-1-Q; E-2-Q; IG-1-Q; IG-2-Q
-    && (CollateralClass.isHQLALevel2B flow.collateralClass)
-    -- Forward Start Amount: NULL
-    && (flow.forwardStartAmount == Nothing)
-    -- Forward Start Bucket: NULL
-    && (flow.forwardStartBucket == Nothing)
-    -- Encumbrance Type: Null
-    && (flow.encumbranceType == Nothing)
-    -- Treasury Control: Y
-    && (flow.treasuryControl == True)
+        -- Sub-Product: Not Currency and Coin
+        && (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
+        -- Collateral Class: E-1-Q; E-2-Q; IG-1-Q; IG-2-Q
+        && CollateralClass.isHQLALevel2B flow.collateralClass
+        -- Forward Start Amount: NULL
+        && (flow.forwardStartAmount == Nothing)
+        -- Forward Start Bucket: NULL
+        && (flow.forwardStartBucket == Nothing)
+        -- Encumbrance Type: Null
+        && (flow.encumbranceType == Nothing)
+        -- Treasury Control: Y
+        && (flow.treasuryControl == True)
 
 
 {-| (107) Financial and Central Bank Cash Inflow Amount (§.33(d)(1))
@@ -159,12 +178,11 @@ match_rule_1_section_20_c_1 flow =
 match_rule_107_section_33_d_1 : Assets -> Bool
 match_rule_107_section_33_d_1 flow =
     List.member flow.product [ i_A_3 ]
-    -- Maturity Bucket: <= 30 calendar days but not Open
-    && (MaturityBucket.isLessThanOrEqual30Days flow.maturityBucket && not (MaturityBucket.isOpen flow.maturityBucket))
-    -- Collateral Class: A-0-Q
-    && (CollateralClass.isCash flow.collateralClass)
-    -- Forward Start Amount: NULL
-    && (flow.forwardStartAmount == Nothing)
-    -- Forward Start Bucket: NULL
-    && (flow.forwardStartBucket == Nothing)
-
+        -- Maturity Bucket: <= 30 calendar days but not Open
+        && (MaturityBucket.isLessThanOrEqual30Days flow.maturityBucket && not (MaturityBucket.isOpen flow.maturityBucket))
+        -- Collateral Class: A-0-Q
+        && CollateralClass.isCash flow.collateralClass
+        -- Forward Start Amount: NULL
+        && (flow.forwardStartAmount == Nothing)
+        -- Forward Start Bucket: NULL
+        && (flow.forwardStartBucket == Nothing)
