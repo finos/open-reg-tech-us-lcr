3ccad875d4d89bec75070674c1a92cf1ad55efe6
Stephen Goldbaum <129341+stephengoldbaum@users.noreply.github.com>
2022-11-22 19:05:48 -0500

    Aggregation of rules across Assets

--- src/Regulation/US/LCR/Inflows/Assets.elm
+++ src/Regulation/US/LCR/Inflows/Assets.elm
@@ -12,46 +12,110 @@
 -}
 
 
-module Regulation.US.LCR.Inflows.Assets exposing (..)
+module Regulation.US.LCR.Inflows.Assets exposing (applyRules, sumRules)
 
+import Morphir.SDK.Aggregate as Agg exposing (..)
 import Regulation.US.FR2052A.DataTables.Inflows.Assets exposing (..)
 import Regulation.US.FR2052A.Fields.CollateralClass as CollateralClass
 import Regulation.US.FR2052A.Fields.Insured as Insured
 import Regulation.US.FR2052A.Fields.MaturityBucket as MaturityBucket
-import Regulation.US.FR2052A.Fields.SubProduct as SubProduct
+import Regulation.US.FR2052A.Fields.SubProduct as SubProduct exposing (currency_and_coin)
 import Regulation.US.LCR.AmountCalculations exposing (..)
 import Regulation.US.LCR.Basics exposing (Balance)
 import Regulation.US.LCR.Rule exposing (applyRule)
 
 
-type alias AssetAggregations =
-    { rule_1_section_20_a_1_C : Balance
-    , rule_1_section_20_a_1 : Balance
-    , rule_1_section_20_b_1 : Balance
-    , rule_1_section_20_c_1 : Balance
-    , rule_107_section_33_d_1 : Balance
-    }
+{-| Given a list of Assets, applies the applicable rule for each assets, applies the appropriate haircut for the rule,
+and sums the results.
+
+TODO: Apply the haircuts
+
+-}
+type alias Flow =
+    { label : String, value : Float }
+
+
+sumToRule : List Assets -> List Flow
+sumToRule assetsList =
+    assetsList
+        |> List.map
+            (\asset ->
+                { label =
+                    if match_rule_1_section_20_a_1_C asset then
+                        "20(a)(1)-C"
+
+                    else if match_rule_1_section_20_a_1 asset then
+                        "20(a)(1)"
+
+                    else if match_rule_1_section_20_b_1 asset then
+                        "20(b)(1)"
+
+                    else if match_rule_1_section_20_c_1 asset then
+                        "20(c)(1)"
+
+                    else if match_rule_107_section_33_d_1 asset then
+                        "33(d)(1)"
+
+                    else
+                        ""
+                , value = asset.marketValue
+                }
+            )
+        |> List.filter (\a -> a.label == "")
+        |> Agg.groupBy .label
+        |> Agg.aggregate
+            (\key values ->
+                { label = key
+                , value = values (sumOf .value)
+                }
+            )
 
 
 
---applyRules : List Assets -> AssetAggregations
---applyRules assets =
---    AssetAggregations
---        (rule_1_section_20_a_1_C assets)
---        (rule_1_section_20_a_1 assets)
---        (rule_1_section_20_b_1 assets)
---        (rule_1_section_20_c_1 assets)
---        (rule_107_section_33_d_1 assets)
+--sumToRule : List Assets -> List Flow
+--sumToRule assetsList =
+--    assetsList
+--        |> List.map
+--            (\asset ->
+--                { flows =
+--                    if match_rule_1_section_20_a_1_C asset then
+--                        Flow "20(a)(1)-C" asset.marketValue
+--
+--                    else if match_rule_1_section_20_a_1 asset then
+--                        Flow "20(a)(1)" asset.marketValue
+--
+--                    else if match_rule_1_section_20_b_1 asset then
+--                        Flow "20(b)(1)" asset.marketValue
+--
+--                    else if match_rule_1_section_20_c_1 asset then
+--                        Flow "20(c)(1)" asset.marketValue
+--
+--                    else if match_rule_107_section_33_d_1 asset then
+--                        Flow "33(d)(1)" asset.marketValue
+--
+--                    else
+--                        Flow "" 0
+--                }
+--            )
+--        |> List.map .flows
+--        |> List.filter (\a -> a.label == "")
+--        |> Agg.groupBy .label
+--        |> Agg.aggregate
+--            (\key values ->
+--                { label = key
+--                , value = values (sumOf .value)
+--                }
+--            )
 
 
 applyRules : Assets -> List ( String, Float )
 applyRules flow =
     List.concat
-        [ applyRule (match_rule_1_section_20_a_1_C flow) "33(c)" flow.marketValue
-        , applyRule (match_rule_1_section_20_a_1 flow) "33(d)(1)" flow.marketValue
-        , applyRule (match_rule_1_section_20_b_1 flow) "33(d)(2)" flow.marketValue
-        , applyRule (match_rule_1_section_20_c_1 flow) "33(d)(2)" flow.marketValue
-        , applyRule (match_rule_107_section_33_d_1 flow) "33(d)(2)" flow.marketValue
+        [ applyRule (match_rule_1_section_20_a_1_C flow) "20(a)(1)-C" flow.marketValue
+        , applyRule (match_rule_1_section_20_a_1 flow) "20(a)(1)" flow.marketValue
+        , applyRule (match_rule_1_section_20_b_1 flow) "20(b)(1)" flow.marketValue
+        , applyRule (match_rule_1_section_20_c_1 flow) "20(c)(1)" flow.marketValue
+        , applyRule (match_rule_107_section_33_d_1 flow) "33(d)(1)" flow.marketValue
         ]
 
 
@@ -101,11 +165,12 @@ match_rule_1_section_20_a_1_C : Assets -> Bool
 match_rule_1_section_20_a_1_C flow =
     List.member flow.product [ i_A_3 ]
         -- Sub-Product: Not Currency and Coin
-        && (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
-        -- Maturity Bucket: Open
-        && MaturityBucket.isOpen flow.maturityBucket
-        -- Collateral Class: A-0-Q
-        && CollateralClass.isCash flow.collateralClass
+        --&& (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
+        && (flow.subProduct /= Just currency_and_coin)
+        ---- Maturity Bucket: Open
+        --&& MaturityBucket.isOpen flow.maturityBucket
+        ---- Collateral Class: A-0-Q
+        --&& CollateralClass.isCash flow.collateralClass
         -- Forward Start Amount: NULL
         && (flow.forwardStartAmount == Nothing)
         -- Forward Start Bucket: NULL
@@ -122,9 +187,10 @@ match_rule_1_section_20_a_1 : Assets -> Bool
 match_rule_1_section_20_a_1 flow =
     List.member flow.product [ i_A_1, i_A_2 ]
         -- Sub-Product: Not Currency and Coin
-        && (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
-        -- Collateral Class: A-1-Q; A-2-Q; A-3-Q; A-4-Q; A-5-Q; S-1-Q; S-2-Q; S-3-Q; S-4-Q; CB-1-Q; CB-2-Q
-        && (CollateralClass.isHQLALevel1 flow.collateralClass && not (CollateralClass.isCash flow.collateralClass))
+        --&& (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
+        && (flow.subProduct /= Just currency_and_coin)
+        ---- Collateral Class: A-1-Q; A-2-Q; A-3-Q; A-4-Q; A-5-Q; S-1-Q; S-2-Q; S-3-Q; S-4-Q; CB-1-Q; CB-2-Q
+        --&& (CollateralClass.isHQLALevel1 flow.collateralClass && not (CollateralClass.isCash flow.collateralClass))
         -- Forward Start Amount: NULL
         && (flow.forwardStartAmount == Nothing)
         -- Forward Start Bucket: NULL
@@ -141,9 +207,10 @@ match_rule_1_section_20_b_1 : Assets -> Bool
 match_rule_1_section_20_b_1 flow =
     List.member flow.product [ i_A_1, i_A_2 ]
         -- Sub-Product: Not Currency and Coin
-        && (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
-        -- Collateral Class: G-1-Q; G-2-Q; G-3-Q; S-5-Q; S-6-Q; S-7-Q; CB-3-Q
-        && CollateralClass.isHQLALevel2A flow.collateralClass
+        --&& (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
+        && (flow.subProduct /= Just currency_and_coin)
+        ---- Collateral Class: G-1-Q; G-2-Q; G-3-Q; S-5-Q; S-6-Q; S-7-Q; CB-3-Q
+        --&& CollateralClass.isHQLALevel2A flow.collateralClass
         -- Forward Start Amount: NULL
         && (flow.forwardStartAmount == Nothing)
         -- Forward Start Bucket: NULL
@@ -160,9 +227,10 @@ match_rule_1_section_20_c_1 : Assets -> Bool
 match_rule_1_section_20_c_1 flow =
     List.member flow.product [ i_A_1, i_A_2 ]
         -- Sub-Product: Not Currency and Coin
-        && (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
-        -- Collateral Class: E-1-Q; E-2-Q; IG-1-Q; IG-2-Q
-        && CollateralClass.isHQLALevel2B flow.collateralClass
+        --&& (flow.subProduct |> Maybe.map (\subProduct -> not (SubProduct.isCurrencyAndCoin subProduct)) |> Maybe.withDefault True)
+        && (flow.subProduct /= Just currency_and_coin)
+        ---- Collateral Class: E-1-Q; E-2-Q; IG-1-Q; IG-2-Q
+        --&& CollateralClass.isHQLALevel2B flow.collateralClass
         -- Forward Start Amount: NULL
         && (flow.forwardStartAmount == Nothing)
         -- Forward Start Bucket: NULL
@@ -179,9 +247,9 @@ match_rule_107_section_33_d_1 : Assets -> Bool
 match_rule_107_section_33_d_1 flow =
     List.member flow.product [ i_A_3 ]
         -- Maturity Bucket: <= 30 calendar days but not Open
-        && (MaturityBucket.isLessThanOrEqual30Days flow.maturityBucket && not (MaturityBucket.isOpen flow.maturityBucket))
-        -- Collateral Class: A-0-Q
-        && CollateralClass.isCash flow.collateralClass
+        --&& (MaturityBucket.isLessThanOrEqual30Days flow.maturityBucket && not (MaturityBucket.isOpen flow.maturityBucket))
+        ---- Collateral Class: A-0-Q
+        --&& CollateralClass.isCash flow.collateralClass
         -- Forward Start Amount: NULL
         && (flow.forwardStartAmount == Nothing)
         -- Forward Start Bucket: NULL
