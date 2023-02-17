ce1ac18abfabdc7425ea9fce02986fa44751ef14
Stephen Goldbaum <129341+stephengoldbaum@users.noreply.github.com>
2023-02-17 10:32:09 -0500

    Fixes for Scala generation and code visibility

---
+++ src/Regulation/US/LCR/Rules.elm
@@ -0,0 +1,43 @@
+{-
+   Copyright 2022 Morgan Stanley
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+-}
+
+
+module Regulation.US.LCR.Rules exposing (..)
+
+import Regulation.US.LCR.Basics exposing (Balance)
+
+
+type alias Weight =
+    Float
+
+
+type alias RuleName =
+    String
+
+
+type alias RuleBalance =
+    { rule : RuleName
+    , amount : Balance
+    }
+
+findAll : List RuleName -> List RuleBalance -> List RuleBalance
+findAll rules flows =
+    flows
+        |> List.filter (\ruleBalance -> List.member ruleBalance.rule rules)
+
+
+matchAndSum : List String -> List RuleBalance -> Balance
+matchAndSum rules flows =
+    findAll rules flows
+        |> List.map .amount
+        |> List.sum
